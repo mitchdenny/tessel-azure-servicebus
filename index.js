f0687a416d677c010fd0e787ce270e9dc63b208c
Mitch Denny <mitch@mitchdenny.com>
2014-09-17 22:59:10 +1000

    Built out a client API with no implementation.

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// Copyright 2014 Mitch Denny
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 module.exports = function() {
-    console.log('Hello world');
+    createQueueClient: function(namespace) {
+    	return {
+    		createQueue: function(path) {
+    			console.error('createQueue(path) has not been implemented yet.');
+    			return {};
+    		};
+
+    		deleteQueue: function(path) {
+    			console.error('deleteQueue(path) has not been implemented yet.')
+    			return {};
+    		};
+
+    		getQueue: function(path) {
+    			console.error('getQueue(path) has not been implemented yet.');
+    			return {};
+    		};
+
+    		listQueues: function() {
+    			console.error('listQueues() has not been implemented yet.');
+    			return {};
+    		};
+    	};
+    };
+    }
 }
