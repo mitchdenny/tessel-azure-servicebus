f39ebd9f2e1aa81093c06f39ce44889a4319451b
Mitch Denny <mitch@mitchdenny.com>
2014-09-18 22:50:22 +1000

    Added in Grunt and copied around some licensing headers.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var tasb = require("../index.js");
-var assert = require('assert');
-describe('tessel-azure-servicebus', function(){
-	describe('#createQueueClient', function() {
-		it ('should return a QueueClient with a createQueue function.', function () {
-			var queueClient = tasb.createQueueClient('tasbunittest');
-			assert(queueClient.createQueue != null, 'QueueClient does not contain a defintiion for createQueue.');
-		});
-	});
-});
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  grunt.registerTask('default', []);
+};
